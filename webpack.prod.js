8fe3659276d3306a1da07193deaf642e9674c865
Amit <amit11893@gmail.com>
2020-12-10 15:57:26 +0530

    deploy

---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g)/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {},
+    }),
+  ],
+};
