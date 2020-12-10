86538aa14d62b940160a1e1811a78f2ac0b69c5d
Amit <amit11893@gmail.com>
2020-12-10 16:51:59 +0530

    deploy

--- src/fetchData.js
+++ src/fetchData.js
@@ -7,9 +7,7 @@ import {
 function fetchData(url) {
   return (dispatch) => {
     dispatch(fetchDataPending());
-    fetch(
-      'https://app-d78e8da9-5094-4938-9d72-4db84f2789ef.cleverapps.io' + url
-    )
+    fetch('https://got-back.herokuapp.com' + url)
       .then((res) => res.json())
       .then((res) => {
         if (res.error) {
