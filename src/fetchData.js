abe43f7767c9e9a1ff48020d5a6cc252511b0989
Amit <amit11893@gmail.com>
2020-12-11 11:17:13 +0530

    url

--- src/fetchData.js
+++ src/fetchData.js
@@ -7,7 +7,7 @@ import {
 function fetchData(url) {
   return (dispatch) => {
     dispatch(fetchDataPending());
-    fetch(`${process.env.BACKEND_URL || 'html://localhost:8080'}${url}`)
+    fetch(`${'https://got-back.herokuapp.com'}${url}`)
       .then((res) => res.json())
       .then((res) => {
         if (res.error) {
